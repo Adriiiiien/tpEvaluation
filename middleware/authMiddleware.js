2ceb21b633528d267cd1417f7c020b19a9b61f5f
Adrien <adrien-simon@outlook.com>
2025-09-15 10:12:06 +0200

    authmiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // "Bearer TOKEN"
+    const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) return res.status(401).json({ message: 'Token manquant' });
 
     try {
         const user = jwt.verify(token, process.env.JWT_SECRET);
         req.user = user; // stocke l'ID et le rôle de l'utilisateur
-        next(); // passe au controller
+        next();
     } catch {
         return res.status(403).json({ message: 'Token invalide' });
     }
